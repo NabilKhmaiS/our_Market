6885d5f84cf7f1934862ae98f1ead2554e7db7af
NabilKhamiS <nabilkhamis2n@gmail.com>
2024-12-18 04:12:23 -0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:our_market/features/ui/login_view.dart';
+
+void main() {
+  runApp(const ourMarket());
+}
+
+class ourMarket extends StatelessWidget {
+  const ourMarket({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginView(),
+      title: 'our Market',
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}

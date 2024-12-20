36f68439086dadc31cfaf7c915b9d4674899686b
NabilKhamiS <nabilkhamis2n@gmail.com>
2024-12-20 06:42:08 -0800

    create app views

--- lib/features/home/main_home_view.dart
+++ lib/features/home/main_home_view.dart
@@ -1,27 +1,38 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:our_market/cor/app_colors.dart';
+import 'package:our_market/features/home/views/nav_bar/ui/Profile%20views/Profile_view.dart';
+import 'package:our_market/features/home/views/nav_bar/ui/Stor_views/store_views.dart';
+import 'package:our_market/features/home/views/nav_bar/ui/favorite%20views/favorite_view.dart';
+import 'package:our_market/features/home/views/nav_bar/ui/home%20views/home_view.dart';
 
 class MainHomeView extends StatelessWidget {
-  const MainHomeView({super.key});
+   MainHomeView({super.key});
 
+  final List<Widget> views = [
+    HomeView(),
+    StoreViews(),
+    FavoriteView(),
+    ProfileView(),
+
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(
-          child: Text('test'),
-        ),
-      )),
+      body: SafeArea(child: views[2]),
       bottomNavigationBar: Container(
         decoration: BoxDecoration(
           color: AppColors.kWhiteColor
         ),
         child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8,horizontal: 15),
+          padding:  EdgeInsets.symmetric(vertical: 8,horizontal: 15),
           child: GNav(
+            onTabChange: (value) {
+              log(value.toString() as num);
+            },
+
               rippleColor: AppColors.kPrimaryColor, // tab button ripple color when pressed
               hoverColor: AppColors.kPrimaryColor, // tab button hover color
               //haptic: true, // haptic feedback
---
+++ lib/features/home/views/nav_bar/ui/Stor_views/store_views.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StoreViews extends StatelessWidget {
+  const StoreViews({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("stor"),);
+  }
+}

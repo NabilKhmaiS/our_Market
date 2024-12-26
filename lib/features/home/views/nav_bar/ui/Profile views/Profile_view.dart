d8590b74bf96d1200346cb34c1888ada654614d7
NabilKhamiS <nabilkhamis2n@gmail.com>
2024-12-26 11:14:59 -0800

    finish profile ui

--- lib/features/home/views/nav_bar/ui/Profile views/Profile_view.dart
+++ lib/features/home/views/nav_bar/ui/Profile views/Profile_view.dart
@@ -1,68 +1,74 @@
 import 'package:flutter/material.dart';
 import 'package:our_market/cor/app_colors.dart';
+import 'package:our_market/features/home/views/nav_bar/ui/Profile%20views/widgets/CustomRowBtn.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(24),
-      color: AppColors.kWhiteColor,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              CircleAvatar(
-                radius: 50,
-                backgroundColor: AppColors.kPrimaryColor,
-                foregroundColor: AppColors.kWhiteColor,
-                child: Icon(Icons.person,size: 35,),
-              ),
-              SizedBox(height: 16),
-              Text(
-                'User name',
-                style: TextStyle( fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 10),
-              Text(
-                ' User Email',
-                style: TextStyle(),
-              ),
-              Card(
-              color: AppColors.kPrimaryColor,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(16)),
-                  
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Icon(Icons.person,color: AppColors.kWhiteColor,),
-
-                      Text("Edit Name",style: TextStyle(color: AppColors.kWhiteColor),),
-                      Icon(Icons.arrow_forward,color: AppColors.kWhiteColor)
-
-                    ],
-                  ),
-                ),
-              )
-
-
-
-
-
-            ],
+    return Center(
+      child: SizedBox(
+       height: MediaQuery.sizeOf(context).height * .65,
+        child: Card(
+          margin: const EdgeInsets.all(24),
+          color: AppColors.kWhiteColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
           ),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundColor: AppColors.kPrimaryColor,
+                    foregroundColor: AppColors.kWhiteColor,
+                    child: Icon(Icons.person,size: 35,),
+                  ),
+                  SizedBox(height: 16),
+                  Text(
+                    'User name',
+                    style: TextStyle( fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    ' User Email',
+                    style: TextStyle(),
+                  ),
+                  SizedBox(height: 10,),
+                  CustomRowBtn(
+                 onTap: (){},
+                    icon: Icons.person,
+                    label: 'Edit Profile',
+
+
+                  ),
+                  SizedBox(height: 10,),
+                  CustomRowBtn(
+                  onTap: (){},
+                    icon: Icons.shopping_basket,
+                    label: 'My Orders',),
+                  SizedBox(height: 10,),
+                  CustomRowBtn(
+                  onTap: (){},
+                    icon: Icons.logout,
+                    label: 'Logout',),
+
+
+
+
+
+                ],
+              ),
+            ),
+          ),
+
         ),
       ),
-
     );
   }
 }
+
+
---
+++ lib/features/home/views/nav_bar/ui/Profile views/widgets/CustomRowBtn.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:our_market/cor/app_colors.dart';
+
+class CustomRowBtn extends StatelessWidget {
+  const CustomRowBtn({
+    super.key, this.onTap, required this.icon, required this.label,
+  });
+  @override
+  final void Function()? onTap;
+  final IconData icon;
+  final String label;
+
+  Widget build(BuildContext context) {
+    return Card(
+      color: AppColors.kPrimaryColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(16)),
+
+      ),
+      child: InkWell(
+        onTap: onTap,
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Icon(Icons.person,color: AppColors.kWhiteColor,),
+
+              Text(label,style: TextStyle(color: AppColors.kWhiteColor,fontWeight: FontWeight.bold),),
+              Icon(icon,color: AppColors.kWhiteColor)
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

f0ba2ea999cc854986a49ee669fd22250207e0d4
NabilKhamiS <nabilkhamis2n@gmail.com>
2024-12-26 10:49:53 -0800

    create card profile view

--- lib/features/home/views/nav_bar/ui/Profile views/Profile_view.dart
+++ lib/features/home/views/nav_bar/ui/Profile views/Profile_view.dart
@@ -1,10 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:our_market/cor/app_colors.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("ProfileView"),);
+    return Card(
+      margin: const EdgeInsets.all(24),
+      color: AppColors.kWhiteColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundColor: AppColors.kPrimaryColor,
+                foregroundColor: AppColors.kWhiteColor,
+                child: Icon(Icons.person,size: 35,),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'User name',
+                style: TextStyle( fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              Text(
+                ' User Email',
+                style: TextStyle(),
+              ),
+              Card(
+              color: AppColors.kPrimaryColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(16)),
+                  
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(Icons.person,color: AppColors.kWhiteColor,),
+
+                      Text("Edit Name",style: TextStyle(color: AppColors.kWhiteColor),),
+                      Icon(Icons.arrow_forward,color: AppColors.kWhiteColor)
+
+                    ],
+                  ),
+                ),
+              )
+
+
+
+
+
+            ],
+          ),
+        ),
+      ),
+
+    );
   }
 }

970535b347fb7173a5a657396605999103968abb
NabilKhamiS <nabilkhamis2n@gmail.com>
2024-12-26 10:12:17 -0800

    finish stor views

--- lib/features/home/views/nav_bar/ui/Stor_views/store_views.dart
+++ lib/features/home/views/nav_bar/ui/Stor_views/store_views.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:our_market/cor/components/custom_search_field.dart';
+import 'package:our_market/cor/components/products-List.dart';
 
 class StoreViews extends StatelessWidget {
   const StoreViews({super.key});
@@ -11,7 +12,15 @@ class StoreViews extends StatelessWidget {
         padding: const EdgeInsets.all(8.0),
         child: ListView(
           children: [
+            Center(
+              child: Text("Welcome to our Market",style:
+                TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+            ),
+            SizedBox(height: 15,),
             CustomSearchField(),
+            SizedBox(height: 15,),
+            Productslist(),
+
 
           ],
         ),
